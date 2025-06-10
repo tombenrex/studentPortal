1c2d2e2229d26b4926f69774d5eec859ce5096b1
Tom Larsson <tmlsn@hotmail.com>
2025-06-10 13:03:14 +0200

    Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,39 +2,35 @@ import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <header className='section-hero hero'>
-      <div className='white-overlay'></div>
+    <main>
+      <section className='section-hero hero d-flex align-items-center justify-content-center min-vh-100'>
+        <div className='white-overlay'></div>
 
-      <div className='container position-relative z-2'>
-        <h1 className='display-1 fw-bold header'>
-          Vi firar examen av
-          <br />
-          Studenterna 2025
-        </h1>
+        <div className='container position-relative z-2 text-center'>
+          <h1 className='display-4 fw-bold header'>
+            Vi firar examen av
+            <br />
+            Studenterna 2025
+          </h1>
 
-        <div className='mt-4'>
-          <Link to='/courses' className='btn btn-danger btn-lg me-3'>
-            <i className='bi bi-arrow-right-circle me-2'></i>
-            Se våra program
-          </Link>
+          <div className='mt-4'>
+            <Link to='/courses' className='btn btn-danger btn-lg me-md-3 mb-3 mb-md-0'>
+              <i className='bi bi-arrow-right-circle me-2'></i>
+              Se våra program
+            </Link>
+          </div>
+
+          <div className='highlight-box p-4 mt-4 mx-auto'>
+            <div className='bg-primary shadow rounded p-3'>
+              <h2 className='h5'>Håvard anses vara den populäraste skolan</h2>
+              <Link to='/news' className='btn btn-outline-dark mt-3'>
+                <i className='bi bi-arrow-right me-2'></i>
+                Läs mer i nyheterna
+              </Link>
+            </div>
+          </div>
         </div>
-
-        <div
-          className='position-relative text-center p-4 shadow bg-primary mt-4 mt-md-0 mx-auto mx-md-0'
-          style={{
-            right: '2rem',
-            maxWidth: '400px',
-            zIndex: 3,
-            borderRadius: '0.5rem',
-          }}
-        >
-          <h4>Håvard utses vara den populäraste skolan</h4>
-          <Link to='/news' className='btn btn-outline-dark mt-2'>
-            <i className='bi bi-arrow-right me-2'></i>
-            Läs mer i nyheterna
-          </Link>
-        </div>
-      </div>
-    </header>
+      </section>
+    </main>
   )
 }
